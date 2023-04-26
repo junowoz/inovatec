55ae933ec9913f245c7fa780f2d3bb4f1d37586d
junowoz <junogouvea@gmail.com>
2023-04-26 16:20:08 -0400

    dashboard

---
+++ pages/dashboard/index.js
@@ -0,0 +1,27 @@
+import { Col, Container, Row, Button } from "react-bootstrap";
+import Canva from "./canva";
+import Dashboard from "./dashboard";
+import React from 'react';
+
+export default function Index() {
+
+  return (
+    <Container fluid>
+      <Row>
+        <Col lg="3" xl="3" xxl="3">
+          <Canva></Canva>
+        </Col>
+
+        <Col>
+          <Row style={{ backgroundColor: "rgba(218, 218, 218, 0.184)" }}>
+            <div className="">
+              <Container className="d-flex justify-content-center" fluid>
+                <Dashboard />
+              </Container>
+            </div>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
