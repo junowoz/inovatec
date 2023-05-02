72ddcf5bd9abe5f6a5e2888d6570c62ef9bee3e2
junowoz <junogouvea@gmail.com>
2023-05-02 01:50:35 -0400

    inscrever final

--- pages/inscrever/finalizar/index.js
+++ pages/inscrever/finalizar/index.js
@@ -9,101 +9,74 @@ import { useInscreverState } from "context/InscreverProjetos/InscreverState";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import Head from "next/head";
 
 const schema = yup.object().shape({
-  members: yup
+  member: yup
     .array()
     .of(
       yup.object().shape({
-        name: yup.string().required("Nome é obrigatório"),
-        role: yup.string().required("Função é obrigatória"),
-        showContact: yup.boolean(),
-        contact: yup
-          .string()
-          .email("Email inválido")
-          .when("showContact", {
-            is: true,
-            then: yup.string().required("Contato é obrigatório"),
-            otherwise: yup.string(),
-          }),
+        name: yup.string().required("Nome obrigatório"),
+        email: yup.string().when("isLeader", {
+          is: true,
+          then: yup
+            .string()
+            .required("Email é obrigatório para os líderes")
+            .email("Email inválido"),
+          otherwise: yup.string().email("Email inválido"),
+        }),
+        isLeader: yup.boolean(),
       })
     )
-    .min(1, "Pelo menos um membro com função e contato é necessário")
+    .min(1, "Pelo menos um membro é necessário")
     .required("Membros são obrigatórios"),
 });
 
-
 export default function Finalizar() {
-  const [hydration, setHydration] = useState(true);
   const router = useRouter();
-
-  const {
-    formData,
-    members: membersData,
-    submitData,
-    roleData,
-    fetchData,
-  } = useInscreverState();
+  const [hydration, setHydration] = useState(true);
+  const { formData, submitData, submitMember } = useInscreverState();
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
 
   const {
     register,
+    unregister,
     handleSubmit,
-    setValue,
-    getValues,
     formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-    defaultValues: {
-      members: [{ name: "", role: "", contact: "", showContact: false }],
-    },
-  });
+  } = useForm({ resolver: yupResolver(schema) });
 
-  const [members, setMembers] = useState([
-    { name: "", role: "", contact: "", showContact: false },
+  //ADD, DELETE CARDS, AND SET VALUES
+  const [member, setMember] = useState([
+    { id: randomNumber, name: "", email: "", isLeader: false },
   ]);
 
-  //OTHER FUNCTIONS
-  const handleInputChange = (memberIndex, event) => {
-    const newMembers = [...members];
-    newMembers[memberIndex][event.target.name] = event.target.value;
-    setMembers(newMembers);
-    setValue(
-      `members[${memberIndex}].${event.target.name}`,
-      event.target.value
-    );
-    console.log(newMembers);
-  };
-
   const handleAddClick = () => {
-    const newMember = { name: "", role: "", contact: "", showContact: false };
-    setMembers([...members, newMember]);
-    const newMembers = [
-      ...getValues("members"),
-      newMember,
-    ];
-    setValue("members", newMembers);
+    setMember([
+      ...member,
+      { id: randomNumber, name: "", email: "", isLeader: false },
+    ]);
   };
-  
-  const handleRemoveClick = (index) => {
-    const newMembersLocal = members.filter((_, i) => i !== index);
-    setMembers(newMembersLocal);
-    const newMembersForm = getValues("members").filter((_, i) => i !== index);
-    setValue("members", newMembersForm);
-  };
-  
 
-  //POSTAR TUDO
-  const handleOnSubmit = async (event) => {
+  const handleRemoveClick = (index) => {
+    const list = [...member];
+    list.splice(index, 1);
+    setMember(list);
+
+    // Unregister fields
+    unregister(`member.${index}.name`);
+    unregister(`member.${index}.email`);
+    unregister(`member.${index}.isLeader`);
+  };
+
+  // POSTAR TUDO
+  const onSubmit = async (event) => {
     console.log(event);
-    submitData(formData, members);
+    submitData(formData, member);
     router.push("/inscrever/sucesso");
-    console.log(formData);
-    console.log(members);
   };
 
   //HYDRATION
   useEffect(() => {
-    fetchData();
     setHydration(false);
   }, []);
 
@@ -111,18 +84,28 @@ export default function Finalizar() {
     ""
   ) : (
     <Main>
+      <Head>
+        <title>Inovatec | Inscrever</title>
+      </Head>
       <Container>
         <Row className="justify-content-md-center">
           <Col md="8" xl="6">
-            <Form onSubmit={handleSubmit(handleOnSubmit)} className="py-5">
+            <Form onSubmit={handleSubmit(onSubmit)} className="py-5">
               <Progress2 />
+
+              {/* MENSAGEM */}
               <Alert variant="primary">
                 <Alert.Heading>
                   <FaUserPlus className="me-2" /> Adicione os membros do projeto
                 </Alert.Heading>
                 <p>
-                  Insira os nomes e funções dos membros do projeto abaixo. Você
-                  também pode adicionar um contato para cada membro.
+                  Insira os nomes dos membros do projeto abaixo. Se o membro for
+                  um líder, marque a caixa de seleção "Líder", e insira o email
+                  para contato.
+                </p>
+                <p>
+                  Líderes são os responsáveis pelo projeto, membros são outros
+                  integrantes do projeto.
                 </p>
                 <p>
                   Ao finalizar inscrição, simplesmente clique no botão
@@ -131,127 +114,118 @@ export default function Finalizar() {
               </Alert>
 
               {/* MEMBROS */}
-              {members.map((member, index) => (
-                <Card className="p-3 mb-3" key={index}>
+              {member.map((item, index) => (
+                <Card className="p-3 mb-3" key={item.id}>
                   <Row className="align-items-center">
-                    {/* Aparece o botão de excluir membro apenas em telas pequenas */}
+                    {/* MOBILE DELETE */}
                     <Col lg="1" className="d-block d-lg-none">
                       <Button
                         variant="outline-danger"
                         onClick={() => handleRemoveClick(index)}
+                        disabled={member.length === 1}
                         className="w-xs-0 w-100 mb-2 mb-lg-0"
-                        size="sm"
+                        size="md"
                       >
                         <FaTrash />
                       </Button>
                     </Col>
 
                     {/* NOME DO MEMBRO */}
-                    <Col lg="6">
-                      <Form.Group>
+                    <Col
+                      lg={item.isLeader ? "5" : "10"}
+                      className="mb-2 mb-lg-0"
+                    >
+                      <Form.Group controlId={`name${index}`}>
                         <Form.Control
                           type="text"
-                          name={`members[${index}].name`}
                           placeholder="Nome do membro"
-                          {...register(`members[${index}].name`)}
-                          onChange={(event) => handleInputChange(index, event)}
+                          {...register(`member.${index}.name`)}
+                          value={item.name}
+                          onChange={(e) => {
+                            const updatedMember = [...member];
+                            updatedMember[index].name = e.target.value;
+                            setMember(updatedMember);
+                          }}
                         />
-                        {errors.members &&
-                          errors.members[index] &&
-                          errors.members[index].name && (
-                            <Form.Control.Feedback type="invalid">
-                              {errors.members[index].name.message}
-                            </Form.Control.Feedback>
-                          )}
+                        <Form.Text className="text-danger">
+                          {errors.member &&
+                            errors.member[index] &&
+                            errors.member[index].name?.message}
+                        </Form.Text>
                       </Form.Group>
                     </Col>
 
-                    {/* FUNÇÃO DO MEMBRO */}
-                    <Col lg="5">
-                      <Form.Group className="mt-2 mt-lg-0">
-                        <Form.Select
-                          name={`members[${index}].role`}
-                          {...register(`members[${index}].role`)}
-                          onChange={(event) => handleInputChange(index, event)}
+                    {/* EMAIL PARA CONTATO */}
+                    {item.isLeader && (
+                      <Col lg="5" className="mb-2 mb-lg-0 mr-3">
+                        <Form.Group
+                          controlId={`email${index}`}
+                          className="mt-2 m-sm-0"
                         >
-                          <option value="">Escolher função</option>
-                          {roleData.map((role) => (
-                            <option key={role.id} value={role.id}>
-                              {role.name}
-                            </option>
-                          ))}
-                        </Form.Select>
-                        {errors.members &&
-                          errors.members[index] &&
-                          errors.members[index].role && (
-                            <Form.Control.Feedback type="invalid">
-                              {errors.members[index].role.message}
-                            </Form.Control.Feedback>
-                          )}
-                      </Form.Group>
-                    </Col>
+                          <Form.Control
+                            type="email"
+                            placeholder="digite@email.com"
+                            {...register(`member.${index}.email`)}
+                            value={item.email}
+                            onChange={(e) => {
+                              const updatedMember = [...member];
+                              updatedMember[index].email = e.target.value;
+                              setMember(updatedMember);
+                            }}
+                          />
+                          <Form.Text className="text-danger">
+                            {errors.member &&
+                              errors.member[index] &&
+                              errors.member[index].email?.message}
+                          </Form.Text>
+                        </Form.Group>
+                      </Col>
+                    )}
 
-                    {/* Aparece o botão de excluir membro apenas em telas grandes */}
+                    {/* BOTÃO EXCLUIR MEMBRO */}
                     <Col lg="1" className="d-none d-lg-block">
                       <Button
                         variant="outline-danger"
                         onClick={() => handleRemoveClick(index)}
-                        className="mb-2 mb-lg-0 mx-auto"
-                        size="sm"
+                        disabled={member.length === 1}
+                        size="md"
                       >
                         <FaTrash />
                       </Button>
                     </Col>
                   </Row>
 
-                  {/* ADICIONAR EMAIL PARA CONTATO */}
-                  <Row className="mt-2">
-                    <Col lg="8">
+                  {/* CHECKBOX LÍDER */}
+                  <Row lg="1">
+                    <Form.Group controlId={`isLeader${index}`} className="mt-2">
                       <Form.Check
                         type="checkbox"
-                        label="Adicionar email para contato"
-                        onChange={() => {
-                          const values = [...members];
-                          values[index].showContact =
-                            !values[index].showContact;
-                          setMembers(values);
+                        label="Líder"
+                        {...register(`member.${index}.isLeader`)}
+                        checked={item.isLeader}
+                        onChange={(e) => {
+                          const updatedMember = [...member];
+                          updatedMember[index].isLeader = e.target.checked;
+                          // Unregister email field if not leader
+                          if (!e.target.checked) {
+                            updatedMember[index].email = "";
+                            unregister(`member.${index}.email`);
+                          }
+                          setMember(updatedMember);
                         }}
                       />
-                    </Col>
+                    </Form.Group>
                   </Row>
-
-                  {/* EMAIL PARA CONTATO */}
-
-                  {member.showContact && (
-                    <Row className="mt-2">
-                      <Col lg="11">
-                        <Form.Group>
-                          <Form.Control
-                            type="email"
-                            name={`members[${index}].contact`}
-                            placeholder="Digite seu email"
-                            {...register(`members[${index}].contact`)}
-                            onChange={(event) =>
-                              handleInputChange(index, event)
-                            }
-                          />
-                          {errors.members &&
-                            errors.members[index] &&
-                            errors.members[index].contact && (
-                              <Form.Control.Feedback type="invalid">
-                                {errors.members[index].contact.message}
-                              </Form.Control.Feedback>
-                            )}
-                        </Form.Group>
-                      </Col>
-                    </Row>
-                  )}
                 </Card>
               ))}
               <Row className="align-items-center mt-2">
-                <Col md="8"></Col>
+                <Col xl="8"></Col>
                 <Col md="4">
-                  <Button variant="outline-primary" onClick={handleAddClick}>
+                  <Button
+                    variant="outline-primary"
+                    style={{ minWidth: "150px" }}
+                    onClick={handleAddClick}
+                  >
                     <FaUserPlus className="me-1" /> Adicionar membro
                   </Button>
                 </Col>
