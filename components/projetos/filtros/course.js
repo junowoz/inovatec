660457bf37b7f620a9eb21dacac9844a978aa0a6
junowoz <junogouvea@gmail.com>
2023-05-02 19:30:12 -0400

    delete

--- components/projetos/filtros/course.js
+++
@@ -1,8 +0,0 @@
-const course = [
-  { id: 1, label: "Engenharia da Computação", all: true },
-  { id: 2, label: "Engenharia de Software", all: true },
-  { id: 3, label: "Análise e Desenvolvimento de Sistemas", all: true },
-  { id: 4, label: "Sistema de Informação", all: true },
-];
-
-export default course;
--- components/projetos/filtros/year.js
+++
@@ -1,8 +0,0 @@
-const year = [
-  { id: 1, label: "2023", all: true },
-  { id: 2, label: "2022", all: true },
-  { id: 3, label: "2021", all: true },
-  { id: 4, label: "2020", all: true },
-];
-
-export default year;
