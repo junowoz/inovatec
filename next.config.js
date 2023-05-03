11c6c6befe1395467ecd5c16b2ed8d40d5267591
junowoz <junogouvea@gmail.com>
2023-05-03 02:19:49 -0400

    base

--- next.config.js
+++ next.config.js
@@ -1,20 +1,10 @@
 /** @type {import('next').NextConfig} */
-const  nextConfig = {
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.supabase.co',
-        pathname: '/storage/v1/object/public/imagens/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.google.com',
-      },
-    ],
+    domains: ["tskpdujrzwsmbmdcxlej.supabase.co"],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
