f91850cbd6e3c22ace22d84b877a996bddb08d1c
junowoz <junogouvea@gmail.com>
2023-05-24 19:31:19 -0400

    filtro context

--- context/useFiltroState.js
+++ context/useFiltroState.js
@@ -1,36 +1,39 @@
-// import { supabase } from "supabase/client";
 import create from "zustand";
-import { persist } from "zustand/middleware";
 
 // filtroState.js
 export const useFiltroState = create(
-  persist(
-    (set) => ({
-      filters: {
-        year: "all",
-        course: "all",
-        semester: "all",
-        industry: "all",
-        tech: "all",
-      },
-      setFilter: (filterType, value) =>
-        set((state) => ({
-          filters: { ...state.filters, [filterType]: value },
-        })),
+  (set) => ({
+    filters: {
+      year: [],
+      course: [],
+      semester: [],
+      industry: [],
+      tech: [],
+    },
+    setFilter: (category, id) =>
+      set((state) => {
+        const newCategory = [...state.filters[category]];
+        if (!newCategory.includes(id)) {
+          newCategory.push(id);
+        } else {
+          const index = newCategory.indexOf(id);
+          newCategory.splice(index, 1);
+        }
+        return { filters: { ...state.filters, [category]: newCategory } };
+      }),
 
-      clearFilter: () =>
-        set(() => ({
-          filters: {
-            year: [],
-            course: [],
-            semester: [],
-            industry: [],
-            tech: [],
-          },
-        })),
-    }),
-    {
-      name: "FiltroData",
-    }
-  )
+    clearFilter: () =>
+      set(() => ({
+        filters: {
+          year: [],
+          course: [],
+          semester: [],
+          industry: [],
+          tech: [],
+        },
+      })),
+  }),
+  {
+    name: "FiltroData",
+  }
 );
